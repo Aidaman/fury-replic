75ee3372dc077355dddab616a09a29b1431ee302
bogdmoskv <79306848+bogdmoskv@users.noreply.github.com>
2021-12-15 22:03:01 +0200

    15.12 21:31 bogdmoskv drag-drop

--- src/app/drag-and-drop/drag-and-drop.component.ts
+++ src/app/drag-and-drop/drag-and-drop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-drag-and-drop',
@@ -10,6 +11,23 @@ export class DragAndDropComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+   todo = ['Meeting with Denis', 'Interview scheduled with Frank', 'Party at Rakesh house', 'Interview with Nikita'];
+
+  done = ['Talk to Jeniffer', 'Get stuff done', 'Meet up with new coworkers', 'Have fun at work'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
   }
 
 }
